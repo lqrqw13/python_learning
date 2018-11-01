52e7951ddeb5fce7cb61c0adb9a3b3c8a94a61b6
robert <robert.lqrqw@gmail.com>
2018-11-01 12:18:15 +0800

    add04

---
+++ 05.mini_web/04.实现伪静态_mysql查询/dynamic/mini_frame.py
@@ -0,0 +1,102 @@
+import re
+from pymysql import connect
+
+"""
+URL_FUNC_DICT = {
+    "/index.html": index,
+    "/center.html": center
+}
+"""
+
+URL_FUNC_DICT = dict()
+
+
+def route(url):
+    def set_func(func):
+        # URL_FUNC_DICT["/index.py"] = index
+        URL_FUNC_DICT[url] = func
+        def call_func(*args, **kwargs):
+            return func(*args, **kwargs)
+        return call_func
+    return set_func
+
+
+@route("/index.html")
+def index():
+    with open("./templates/index.html") as f:
+        content = f.read()
+
+    # my_stock_info = "哈哈哈哈 这是你的本月名称....."
+    # content = re.sub(r"\{%content%\}", my_stock_info, content)
+    # 创建Connection连接
+    conn = connect(host='localhost',port=3306,user='root',password='lx131313',database='stock_db',charset='utf8')
+    # 获得Cursor对象
+    cs = conn.cursor()
+    cs.execute("select * from info;")
+    stock_infos = cs.fetchall()
+    cs.close()
+    conn.close()
+    html_template = """
+        <tr>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>
+                <input type="button" value="添加" id="toAdd" name="toAdd" systemidvaule="{}">
+            </td>
+        </tr>"""
+    html=''
+    for info in stock_infos:
+        html += html_template.format(info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[1])
+
+    content = re.sub(r"\{%content%\}", html, content)
+
+    return content
+     
+
+@route("/center.html")
+def center():
+    with open("./templates/center.html") as f:
+        content = f.read()
+
+    my_stock_info = "这里是从mysql查询出来的数据。。。"
+
+    content = re.sub(r"\{%content%\}", my_stock_info, content)
+
+    return content
+
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
+    
+    file_name = env['PATH_INFO']
+    # file_name = "/index.py"
+
+    """
+    if file_name == "/index.py":
+        return index()
+    elif file_name == "/center.py":
+        return center()
+    else:
+        return 'Hello World! 我爱你中国....'
+    """
+
+    try:
+        print(file_name)
+        func = URL_FUNC_DICT[file_name]
+        return func()
+        return
+    except Exception as ret:
+        return "产生了异常：%s" % str(ret)
+
+
+
+
+
+
+
